4bc5aebd632cc85672b1ce5b4f0e60c5b51f6ef1
Joshua Book <joshua.book@gmail.com>
2016-05-20 12:01:45 -0400

    wip

--- app/index.js
+++ app/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { VelocityComponent } from 'velocity-react';
+
+console.log(VelocityComponent);
 
 import './stylesheets/normalize.scss';
 import './stylesheets/app.scss';
+
+const styles = {
+  backgroundColor: 'blue',
+  width: '300px',
+  height: '200px',
+  position: 'absolute',
+}
+
+const Box = () => {
+  return (
+    <div >
+      <div id='box' style={styles}>
+         hello world
+      </div>
+    </div>
+  )
+}
 
 export default class App extends React.Component {
   render () {
     return (
       <div id='app'>
-        <h1>react webpack babel es2015 mocha hmre boilerplate</h1>
-        <p>by booivus</p>
+        <VelocityComponent animation={{top: '200px'}} duration={500}>
+          <Box />
+        </VelocityComponent>
       </div>
     )
   }
